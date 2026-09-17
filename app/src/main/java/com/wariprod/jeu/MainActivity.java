4370f070baf92eea2438a1c5c1849a321c472685
wari-prod <lassinecoulibaly701@gmail.com>
2026-09-17 08:52:28 +0000

    Create MainActivity.java

---
+++ app/src/main/java/com/wariprod/jeu/MainActivity.java
@@ -0,0 +1,24 @@
+package com.wariprod.jeu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/warijeu.html");
+
+        setContentView(webView);
+    }
+                            }
